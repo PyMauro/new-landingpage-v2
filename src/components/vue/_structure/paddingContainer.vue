b23c5bbf1339abd41f38fce5376b9710c2bba3fc
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-02 02:17:19 -0300

    minor(paddingContainer):followed the chatGPT recomendation to put the class in a div wrapper instead of using in the template element

--- src/components/vue/_structure/paddingContainer.vue
+++ src/components/vue/_structure/paddingContainer.vue
@@ -1,5 +1,7 @@
-<template class="relative w-screen h-fit">
-    <div class="w-full h-full px-4 sm:px-8 md:px-16 lg:px-32">
-        <slot></slot>
+<template>
+    <div class="relative w-screen h-fit">
+        <div class="w-full h-full px-4 sm:px-8 md:px-16 lg:px-32">
+            <slot></slot>
+        </div>
     </div>
 </template>
