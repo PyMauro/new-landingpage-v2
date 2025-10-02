a545ca3c00b7a666bc1637b7112818fa00663743
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-02 01:34:23 -0300

    feat(PortafolioSection):added other cards

--- src/components/vue/Landing/PorfolioSection.vue
+++ src/components/vue/Landing/PorfolioSection.vue
@@ -8,13 +8,24 @@
                             class="text-stone-700 pr-8">4.</span>Nuestro Trabajo Habla por Si Solo</h2>
                 </structure-basic-block>
                 <structure-basic-block padding="padding">
-                    <StructureBasicBlock extra-borders="bottom-border" class="bg-neutral-100 rounded-3xl">
-                        <template #no-padding>
-                                <PortafolioCard
-                                    title='Identidad Visual: Romina Macri, expandiendo la cardiología al publico.'
-                                    serviceType='Desarollo Web' />
-                        </template>
-                    </StructureBasicBlock>
+
+                            <div class="w-full flex flex-col gap-2">
+                            <PortafolioCard
+                                title='Identidad Visual: Romina Macri, expandiendo la cardiología al publico.'
+                                serviceType='Desarollo Web, Gestion de Redes' />
+                            <PortafolioCard
+                                title='Branding: Femenina, un negocio amado por sus clientes.'
+                                serviceType='Branding, Desarollo Web, Consultoria' />
+
+                            <PortafolioCard
+                                title='Desarollo Web: Click Informativo, la voz de la sociedad.'
+                                serviceType='Identidad Visual, Desarollo Web' />
+
+                            <PortafolioCard
+                                title='Identidad Visual: SinGlu, comida casera, sin gluten y 100% organica.'
+                                serviceType='Identidad Visual' />
+                            </div>
+
 
                 </structure-basic-block>
             </StructurePaddingContainer>
