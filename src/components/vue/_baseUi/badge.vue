2de55cb8fafe1510ce4c16b816a10a3ba266c676
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-02 00:51:35 -0300

    feat(badge):created badge

---
+++ src/components/vue/_baseUi/badge.vue
@@ -0,0 +1,14 @@
+<template>
+    <span class="inline-flex items-center rounded-3xl  px-4 py-1 text-xs font-medium  text-neutral-700 inset-ring inset-black">
+        <slot></slot>
+    </span>
+</template>
+<script setup>
+const props = defineProps({
+    bgColor: {
+        type: String,
+        required: false,
+        default: "bg-neutral-700"
+    }
+});
+</script>
