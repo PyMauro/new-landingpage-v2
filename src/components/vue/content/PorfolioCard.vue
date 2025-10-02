43cd0ffae30f677c214dd028d9751a0aac15ec90
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-02 01:33:40 -0300

    feat(PortafolioCard.vue): finished Card

--- src/components/vue/content/PorfolioCard.vue
+++ src/components/vue/content/PorfolioCard.vue
@@ -15,19 +15,22 @@ const props = defineProps({
 });
 </script>
 <template>
-    <article class="w-full h-fit flex flex-col gap-4 rounded-3xl p-3 border border-neutral-700">
-        <div class="h-96 w-full aspect-video rounded-xl bg-slate-600">
-        </div>
-        <div class="flex w-full flex-col gap-2 justify-center items-start">
-            <div class="flex gap-1 py-2 w-full flex-wrap">
-                <Badge v-for="service in props.serviceType.split(',')" :key="service">
-                    {{ service }}
-                </Badge>
+    <article class="relative w-full bg-neutral-200 h-fit flex flex-col gap-4 overflow-hidden rounded-3xl p-3 border border-neutral-700">
+        <div class="absolute add-dots left-0 top-0 opacity-70 rounded-full w-full h-full"></div>
+        <div class="w-full h-full z-20">
+            <div class="h-96 w-full aspect-video rounded-xl bg-slate-600">
             </div>
-            <h3 class="font-semibold w-full  text-5xl font-heading text-neutral-700">
-                {{ props.title }}
-            </h3>
-            <BrandButton text="Explorar Proyecto" bg-color="bg-amber-300" />
+            <div class="flex w-full flex-col gap-2 justify-center items-start">
+                <div class="flex gap-1 py-2 w-full flex-wrap">
+                    <Badge v-for="service in props.serviceType.split(',')" :key="service">
+                        {{ service }}
+                    </Badge>
+                </div>
+                <h3 class="font-semibold w-full  text-5xl font-heading text-neutral-700">
+                    {{ props.title }}
+                </h3>
+                <BrandButton text="Explorar Proyecto" bg-color="bg-amber-300" />
+            </div>
         </div>
     </article>
 </template>
