ff70fec6ffee4f0bf1718f96b3cbf9c9e5274430
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-06 01:11:30 -0300

    fix(paddinContainer.vue):replaced w-screen (reason for layout shift in mobile) with w-full

--- src/components/vue/_structure/paddingContainer.vue
+++ src/components/vue/_structure/paddingContainer.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="relative w-screen h-fit">
-        <div class="w-full h-full px-4 sm:px-8 md:px-16 lg:px-32">
+    <div class="relative w-full h-fit">
+        <div class="w-full h-full px-2 sm:px-8 md:px-16 lg:px-32">
             <slot></slot>
         </div>
     </div>
