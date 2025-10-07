a16cc181748aee897bd91a7f8a7d00d5276bbe9e
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-07 17:12:43 -0300

    feat(astro.config):added cloudfare try to allowedHosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,10 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['.trycloudflare.com']
+    }
   },
 
   integrations: [vue(), icon()],
