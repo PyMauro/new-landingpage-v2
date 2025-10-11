9795d18c2b09a8288fb36c2b2328c8af4edf061b
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-11 09:39:00 -0300

    feat(landing,content):added images to PorfolioCard,ServiceCard and their respective implementationcs

--- src/components/vue/Landing/OurServices.vue
+++ src/components/vue/Landing/OurServices.vue
@@ -1,5 +1,6 @@
 <template>    
 <section id="our-services" class="w-full h-full">
+        
             <!-- Our Services -->
     <structure-basic-block extra-borders="bottom-border"  padding="padding">
         <template #no-padding>
@@ -7,11 +8,11 @@
                 <h2 class="font-semibold text-neutral-100 font-heading py-4"><span class="text-stone-700 pr-2 md:pr-8">3.</span>Nuestros Servicios</h2>
         </structure-basic-block>
                 <div class="w-full h-fit flex flex-col gap-2 p-4 md:p-8">
-                        <ServiceCard service-title="Identidad Visual / Branding"/>
-                        <ServiceCard service-title="Edicion de Video" bg-color="bg-amber-300"/>
-                        <ServiceCard service-title="Diseño Grafico" bg-color="bg-emerald-300"/>
-                        <ServiceCard service-title="Desarollo Web" bg-color="bg-violet-300"/>
-                        <ServiceCard service-title="Gestion de Redes" bg-color="bg-black" />
+                        <ServiceCard service-title="Identidad Visual / Branding" url-image="src\assets\services\branding-portada.webp" />
+                        <ServiceCard service-title="Edicion de Video" bg-color="bg-amber-300" url-image="src\assets\services\edicion-de-video-portada .jpg"/>
+                        <ServiceCard service-title="Diseño Grafico" bg-color="bg-emerald-300" url-image="src\assets\services\diseño-grafico-portada.webp" />
+                        <ServiceCard service-title="Desarollo Web" bg-color="bg-violet-300" url-image="src\assets\services\desarollo-web-portada.png"/>
+                        <ServiceCard service-title="Gestion de Redes" bg-color="bg-stone-950" url-image="src\assets\services\gestion-de-redes-portada.png"/>
                 </div>
         </template>
     </structure-basic-block>
--- src/components/vue/content/ServiceCard.vue
+++ src/components/vue/content/ServiceCard.vue
@@ -8,15 +8,21 @@ const props = defineProps({
     bgColor: {
         type: String,
         default: "bg-neutral-100"
+    },
+    urlImage: {
+        type: String,
+        default: ""
     }
+    
 });
 </script>
 <template>
-    <article class="w-full h-fit flex flex-col gap-4 md:flex-row rounded-3xl p-3" :class="props.bgColor">
-        <div class="w-full h-64 max-w-none sm:max-w-3/4 aspect-video rounded-xl bg-slate-600">
+    <article class="w-full relative add-texture overflow-hidden h-fit flex flex-col gap-4 lg:flex-row rounded-3xl p-3" :class="props.bgColor">
+        <img v-if="urlImage" :src=props.urlImage :alt="' Portada de' + props.serviceTitle" class="w-full h-64 max-w-none  lg:max-w-3/4 aspect-video rounded-xl object-cover"/>
+        <div v-else class="w-full h-64 max-w-none sm:max-w-3/4 aspect-video rounded-xl bg-slate-600">
         </div>
         <div class="flex flex-col gap-4 sm:gap-2 justify-center items-start w-full max-w-none md:max-w-1/3">
-            <h3 class="font-semibold font-heading" :class="props.bgColor === 'bg-black' ? 'text-white' : 'text-stone-800'"> 
+            <h3 class="font-semibold font-heading" :class="props.bgColor === 'bg-stone-950' ? 'text-white' : 'text-stone-800'"> 
                 {{ props.serviceTitle }}
             </h3>
             <BrandButton class="w-full md:w-fit" text="Conocer Servicio" bg-color="bg-stone-200" />
