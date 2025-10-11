2a1c53fe4591d63929509e7fc25f73bb5e9e62e9
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-11 09:38:01 -0300

    style(ContactSection.astro):added h1 wrapper in md, modify title padding and number color in Contact title

--- src/components/vue/Landing/HeroSection.vue
+++ src/components/vue/Landing/HeroSection.vue
@@ -8,8 +8,8 @@ import BrandButton from "../Brand/Button.vue";
         <StructureBasicBlock extra-borders="bottom-border" class="py-8">
         <StructureBasicBlock padding="padding">
             <div class="flex flex-col gap-4">
-                <h1 class="font-heading  text-white font-semibold  tracking-[-1.2%] md:max-w-5/6">
-                  <span class="text-amber-400">No existe segunda ocasion, </span>para causar una buena primera impresión.
+                <h1 class="font-heading  text-white font-semibold  tracking-[-1.2%] md:max-w-5/6 text-balance">
+                  <span class="text-amber-400">No existe segunda ocasion, <br class="hidden sm:block"> </span>para causar una buena primera impresión.
                 </h1>
                 <div class="flex flex-col gap-4">
                     <p class="font-body text-lg text-neutral-300 text-balance">Es por eso que debe dejar marca en la mente del consumidor. Esa es mi Especialidad. Asegurarte que tu negocio enamore a primera vista.</p>
