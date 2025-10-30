043264c554efe67887f6e7cb20170276e0363d44
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-30 01:20:48 -0300

    feat(OurServices):split visual identity and add isolated Branding service card

--- src/components/vue/Landing/OurServices.vue
+++ src/components/vue/Landing/OurServices.vue
@@ -8,7 +8,8 @@
                 <h2 class="font-semibold text-neutral-100 font-heading py-4"><span class="text-stone-700 pr-2 md:pr-8">3.</span>Nuestros Servicios</h2>
         </structure-basic-block>
                 <div class="w-full h-fit flex flex-col gap-2 p-4 md:p-8">
-                        <ServiceCard service-title="Identidad Visual / Branding" url-image="src\assets\services\branding-portada.webp" />
+                        <ServiceCard service-title="Identidad Visual" url-image="src\assets\services\branding-portada.webp" />
+                        <ServiceCard service-title="Branding" bg-color="bg-orange-300" url-image="src\assets\services\branding-portada.webp" />
                         <ServiceCard service-title="Edicion de Video" bg-color="bg-amber-300" url-image="src\assets\services\edicion-de-video-portada .jpg"/>
                         <ServiceCard service-title="Diseño Grafico" bg-color="bg-emerald-300" url-image="src\assets\services\diseño-grafico-portada.webp" />
                         <ServiceCard service-title="Desarollo Web" bg-color="bg-violet-300" url-image="src\assets\services\desarollo-web-portada.png"/>
