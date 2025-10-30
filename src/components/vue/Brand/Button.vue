fd78fd5553b05ce8fd51ff5fab33846d6def6fd8
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-10-30 09:41:53 -0300

    feat(Button.vue):add logic to handle click from outside

--- src/components/vue/Brand/Button.vue
+++ src/components/vue/Brand/Button.vue
@@ -1,5 +1,5 @@
 <template>
-     <button
+     <button @click="handleClick"
   class="relative font-medium text-stone-800 bg-stone-800 font-body leading-none p-[1px] rounded-full outline-2 outline-transparent outline-offset-[5px] transform -translate-x-1 -translate-y-1 transition-transform ease-in-out text-center shadow-[0.5px_0.5px_0_0_#292524,1px_1px_0_0_#292524,1.5px_1.5px_0_0_#292524,2px_2px_0_0_#292524,2.5px_2.5px_0_0_#292524,3px_3px_0_0_#292524,0_0_0_2px_#fafaf9,0.5px_0.5px_0_2px_#fafaf9,1px_1px_0_2px_#fafaf9,1.5px_1.5px_0_2px_#fafaf9,2px_2px_0_2px_#fafaf9,2.5px_2.5px_0_2px_#fafaf9,3px_3px_0_2px_#fafaf9,3.5px_3.5px_0_2px_#fafaf9,4px_4px_0_2px_#fafaf9] duration-200 hover:-translate-x-1 hover:-translate-y-1 active:translate-x-0 active:translate-y-0 hover:shadow-[0_0_0_2px_#fafaf9] focus-visible:outline-dashed focus-visible:outline-yellow-400"
 >
   <div
@@ -14,6 +14,8 @@
 </button>
 </template>
 <script setup>
+
+
 const props = defineProps({
   href: {
     type: String,
@@ -28,7 +30,16 @@ const props = defineProps({
     type: String,
     default: 'bg-ambar-300',
   },
-});
+  action: {
+    type: Function,
+  }
+})
+
+const handleClick = () => {
+  console.log("Botón clickeado internamente");
+  if (props.action) props.action(); // ejecuta la función que le pasaron
+}
+;
 
 const btnClassWithBg = `${props.bgColor} relative pointer-events-none border border-white/30 rounded-full before:content-[''] before:absolute before:inset-0 before:rounded-full before:opacity-25 before:bg-[radial-gradient(rgb(255_255_255/_80%)_20%,transparent_20%),radial-gradient(rgb(255_255_255/_100%)_20%,transparent_20%)] before:bg-[length:8px_8px] before:[background-position:0_0,4px_4px] before:mix-blend-hard-light before:animate-[dots_0.5s_linear_infinite]`
 </script>
