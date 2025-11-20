2e5cbcf97775edbc3a4770bd0c098e4e8838221a
Mauro(Hermes) <hermesagenciaweb@gmail.com>
2025-11-20 00:04:56 -0300

    feat(sanityClient,sanityLayout): add union to sanity

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@sanity/client";
-
 export const sanity = createClient({
   projectId: "zahdcxw4",
   dataset: "production",
   apiVersion: "2025-10-01", // o la fecha actual
-  useCdn: true, // true para lecturas rápidas
+  useCdn: false, // true para lecturas rápidas
 });
